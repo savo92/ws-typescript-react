3b6a2bbf0e7a46bd2cc7619811df996a827862f8
Lorenzo Savini <savo@develer.com>
2019-08-29 12:20:12 +0200

    Comments on webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -15,24 +15,33 @@ const isDev = process.env.NODE_ENV !== "production";
 const outputDir = path.resolve("dist");
 
 const plugins = [
+    // To inject data and assets paths to the index.html.
     new HtmlWebpackPlugin({
         inject: false,
         template: "./src/index.html",
         title: "ws-typescript-react",
     }),
+    // To produce a manifest that you can parse to find the real name of a asset file
+    // Useful when the filename suffix is an hash (for cache invalidation purposes usually).
     new ManifestPlugin({ fileName: "manifest.json" }),
+    // To extract styles in dedicated asset files
     new MiniCssExtractPlugin(),
+    // To inject environment variables into our code.
     new webpack.EnvironmentPlugin(["ENVIRONMENT", "NODE_ENV"]),
 ];
 
 module.exports = {
     devtool: isDev ? "cheap-module-source-map" : "source-map",
+
+    // The entrypoint of the TypeScript app.
     entry: "./src/index.tsx",
 
     mode: process.env.NODE_ENV,
 
     module: {
         rules: [
+            // These rules can be unsorted.
+            // They define how to pre-process files, SASS, TypeScript.
             rules.file,
             rules.sass,
             rules.sourceMap,
@@ -42,13 +51,21 @@ module.exports = {
 
     optimization: {
         minimizer: [
+            // Minify final JS.
             new TerserPlugin({
                 cache: true,
                 parallel: true,
                 sourceMap: true,
             }),
+            // Minify CSS.
             new OptimizeCSSAssetsPlugin({}),
         ],
+        // This config enables the split of the code in multiple files (both css and js files).
+        // In this case, we produce a main and a vendor file:
+        // - main contains our source code.
+        // - vendor will contain all the package (from node_modules) that our source code imports.
+        // Useful since our source code should change more often than the node_modules code
+        // Well, as far as our deps are correctly locked (with a yarn.lock, as in this example project).
         splitChunks: {
             chunks: "all",
             minChunks: 1,
@@ -68,6 +85,9 @@ module.exports = {
     },
 
     output: {
+        // "[name]-[chunkhash].min.js" describes how Webpack will compose the filename of the JS bundle files.
+        // This is very useful to enable long-term caching on client, since the hash will invalidate
+        // the cache when a new version (w/ a new hash) is released.
         filename: isDev ? "[name].js" : "[name]-[chunkhash].min.js",
         path: outputDir,
     },
@@ -83,6 +103,7 @@ module.exports = {
         ],
     },
 
+    // Enable some stats that Webpack will send to the CLI output.
     stats: {
         assets: true,
         children: true,
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -2,6 +2,11 @@ const defaultConfig = require("./webpack.config");
 
 const devConfig = Object.assign({}, defaultConfig);
 
+// Enable a local server listening on localhost:8080
+// It will expose our app and live reload (enable by default)
+// will listen for source code changes to rebuild our app and reload the browser page.
+// Here you could also configure a proxy in cases where, as example, your UI
+// is talking to a web server exposed by another port.
 devConfig.devServer = {
     historyApiFallback: true,
     inline: true,
