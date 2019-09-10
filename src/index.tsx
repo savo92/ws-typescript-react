7bb6c1a9402b756be7c81daa06589ab8775e9ab7
Lorenzo Savini <savo@develer.com>
2019-09-10 10:51:58 +0200

    Fix import case

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import * as ReactDOM from "react-dom";
 // tslint:disable-next-line no-import-side-effect
 import "./index.scss";
 
-import { App } from "./App";
+import { App } from "./app";
 
 // JSX syntax is fully supported
 ReactDOM.render(<App />, document.getElementById("app"));
